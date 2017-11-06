7e7c56bc51fe2fbb60765bf4f6de6a65a5fa87ea
CreativeGP <malicktafaminecraft@gmail.com>
2017-11-07 07:38:23 +0900

    init.

---
+++ src/json.php
@@ -0,0 +1,17 @@
+<?php
+/*
+   NUGH Loader - json.php
+   
+   2017:11:07 (yyyy.mm.dd)
+ */
+
+$content = file_get_contents('../examples/testdata.json');
+
+for ($i = 0; $i < strlen($content); $i++) {
+    $char = $content[$i];
+}
+
+class NUGH_JsonLoader {
+    
+}
+?>
